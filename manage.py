822be2ce395b520e56fe47bb86d0398a1d12a273
blackangle0929 <blackangle0929@163.com>
2016-04-06 23:48:18 +0800

    Initial commit

---
+++ manage.py
@@ -0,0 +1,10 @@
+import os
+from app import create_app , db
+from app.models import User,Role
+from flask.ext.script import Manager,Shell
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+
+if __name__ == '__main__':
+	manager.run()
